54e6ffee16a1edf4e97a15d686f6848fcd65ad47
lizzieavila <lizi_avila@yahoo.com.br>
2018-03-13 18:16:59 -0300

    commit

--- wordpress/wp-content/themes/wpcurso/paginageral.php
+++ wordpress/wp-content/themes/wpcurso/paginageral.php
@@ -5,7 +5,7 @@ Template Name: Páginas Gerais
 ?>
 
 <?php get_header(); ?>
-<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt=""/>
+<img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt=""/>
 
 <div class="conteudo-wrapper">
 	<main>
